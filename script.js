b5bdfea36d8ce5834840dd338300ff28e2692066
shravanbhoyar1 <shravanbhoyar3507>
2024-05-18 15:45:03 +0530

    js

---
+++ script.js
@@ -0,0 +1,14 @@
+
+let button= document.querySelector(".btn");
+button.addEventListener("mouseover", function(){
+
+    button.classList.add("div");
+    
+});
+let button1= document.querySelector(".btn");
+button.addEventListener("mouseleave", function(){
+
+    button.classList.add("div");
+    button.textContent= "so cool"
+    
+});
