12e28ef7dc66d66b5f85c264f1a9635697d791c5
shravanbhoyar1 <shravanbhoyar3507>
2024-05-24 12:18:37 +0530

    Second Task Complte

--- script.js
+++ script.js
@@ -7,3 +7,19 @@ p.style.color = "skyblue";
 p.style.fontSize = "45px";
 console.log("hiii");
 })
+
+
+let swapbtn = document.querySelector(".swapimg");
+let img1 = document.querySelector(".img1");
+let img2 = document.querySelector(".img2");
+
+swapbtn.addEventListener("click", function(){
+
+let src1 = img1.src ;
+let src2 = img2.src ;
+
+img1.src = src2;
+img2.src = src1;
+
+
+})
