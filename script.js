67dae1beac949af1fa87714848071b1ec8373428
shravanbhoyar1 <shravanbhoyar3507>
2024-05-25 13:07:11 +0530

    Third Task Complete

--- script.js
+++ script.js
@@ -21,5 +21,22 @@ let src2 = img2.src ;
 img1.src = src2;
 img2.src = src1;
 
+})
+
+let form = document.querySelector("form");
+let inpus = document.querySelectorAll('input[type = "text"]');
+let h4 = document.querySelector("h4");
+
+form.addEventListener("submit", function(ev){
+  ev.preventDefault();
+
+  for(let i=0; i<inpus.length; i++){
+    if(inpus[i].ariaValueMax.trim()===''){
+        h4.textContent = "errrorr";
+        h4.style.color = "rosered";
+        break;
+    }
+  }
 
 })
+
