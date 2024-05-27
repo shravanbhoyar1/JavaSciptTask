878ff12b932bf255b04f20398d84248ec881cb2d
shravanbhoyar1 <shravanbhoyar3507>
2024-05-27 15:44:53 +0530

    task five comple

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+  /* Task-1  */
 let btn = document.querySelector(".task-1");
 let p = document.querySelector("p");
 
@@ -8,7 +10,7 @@ p.style.fontSize = "45px";
 console.log("hiii");
 })
 
-
+  /* Task-2  */
 let swapbtn = document.querySelector(".swapimg");
 let img1 = document.querySelector(".img1");
 let img2 = document.querySelector(".img2");
@@ -23,6 +25,7 @@ img2.src = src1;
 
 })
 
+  /* Task-3  */
 let form = document.querySelector("form");
 let inpus = document.querySelectorAll('input[type = "text"]');
 let h4 = document.querySelector("h4");
@@ -37,6 +40,44 @@ form.addEventListener("submit", function(ev){
         break;
     }
   }
-
 })
 
+  /* Task-4  */
+let addbtn = document.querySelector("#add");
+let removebtn = document.querySelector("#remove");
+let inp = document.querySelector("inps");
+let ul = document.querySelector("ul")
+let li ;
+
+addbtn.addEventListener("click",function(){
+
+  if(inp.value.trim()===''){}
+  else{
+        li = document.createElement("li")
+        li.textContent = inp.value;
+        ul.appendChild(li);
+        inp.value= '';
+  }
+
+})
+removebtn.addEventListener("click",function(){
+ ul.removeChild(li)
+  })
+
+
+    /* Task-5  */
+  let start= document.querySelector("#start");
+  let stop = document.querySelector("#stop");
+  let h3 = document.querySelector("h3");
+  let count=0;
+  let int;
+  start.addEventListener("click", function(){
+   int = setInterval(function(){
+      h3.textContent = count;
+      count++;
+    },1000)
+  })
+  
+   stop.addEventListener("click", function(){
+        clearInterval(int);
+  })
